3387bb566379ec16fce19e923176df24d28b57b0
angelcasaado <108984870+angelcasaado@users.noreply.github.com>
2025-06-06 11:48:19 +0200

    Update Creencias.py

--- src/BDI/Creencias.py
+++ src/BDI/Creencias.py
@@ -1,3 +1,12 @@
+"""
+TFG-Agentes-BDI-Supermercado
+
+Copyright (c) 2025 Angel Casado (https://github.com/angelcasaado)
+
+Este archivo forma parte de TFG-Agentes-BDI-Supermercado.
+Se distribuye bajo la licencia MIT. Para más detalles, consulta el archivo LICENSE
+en la raíz del repositorio.
+"""
 class Creencias:
     def __init__(self):
         self.data = {}
